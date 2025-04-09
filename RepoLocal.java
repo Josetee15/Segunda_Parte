3ad73ed09ae343547a409fd383d590ae8994648a
Jose <joseluisagudoperez@gmail.com>
2025-04-09 16:18:44 +0200

    Segunta parte del ejercicio 4_4

---
+++ RepoLocal.java
@@ -0,0 +1,14 @@
+package ejercicio4_4;
+
+public class RepoLocal {
+
+	public static void main(String[] args) {
+	
+		System.out.println("Hola desde el ejercicio 4.4");
+		System.out.println("Primer cambio desde la 1ª rama.");
+		System.out.println("Tercer cambio de prueba");
+		System.out.println("Ultimo cambio de prueba");
+	
+	}
+
+}
---
+++ RepoLocal2Parte.java
@@ -0,0 +1,10 @@
+package ejercicio4_4;
+
+public class RepoLocal2Parte {
+
+	public static void main(String[] args) {
+
+		System.out.println("Segunta parte Ejercicio 4_4 de Entornos");
+	}
+
+}
