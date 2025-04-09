cff3ff01f8fb22c3affb905ff21e0d9df0586734
Eduu319 <anguloeduardo001@gmail.com>
2025-04-09 17:46:18 +0200

    Añadida una linea para el fork de entornos

--- RepoLocal2Parte.java
+++ RepoLocal2Parte.java
@@ -5,6 +5,7 @@ public class RepoLocal2Parte {
 	public static void main(String[] args) {
 
 		System.out.println("Segunta parte Ejercicio 4_4 de Entornos");
+		System.out.println("Tercera parte desde el fork, Ejercicio 4_4 de Entornos");
 	}
 
 }
